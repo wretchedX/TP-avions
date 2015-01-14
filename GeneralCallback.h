7e3ac8ee24ae4dfb281b3da6260acdb8c0b90d15
Guillaume Moreau <guillaume.moreau@ec-nantes.fr>
2015-01-14 16:11:49 +0100

    creation d'une CMakeLists.txt pour la compilation

--- GeneralCallback.h
+++ GeneralCallback.h
@@ -9,7 +9,7 @@ public:
 	osg::Timer t;
 	osg::Timer_t precedentTour;
 	osg::Timer_t precedentFrame;
-	vector<osg::ref_ptr<osg::PositionAttitudeTransform>> patAvions;
+	vector<osg::ref_ptr<osg::PositionAttitudeTransform> > patAvions;
 	
 	GeneralCallback();
 	virtual void operator()(osg::Node* node, osg::NodeVisitor* nv);
